0bf7d2a1485fc1dac48abd31e651040a21ec3389
Mateusz Cybulski <235847@edu.p.lodz.pl>
2023-05-14 15:17:44 +0200

    Init

---
+++ src/main/java/com/example/bionicmicroservice_establish_priorities/BionicMicroserviceEstablishPrioritiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.bionicmicroservice_establish_priorities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BionicMicroserviceEstablishPrioritiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BionicMicroserviceEstablishPrioritiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bionicmicroservice_establish_priorities/BionicMicroserviceEstablishPrioritiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bionicmicroservice_establish_priorities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BionicMicroserviceEstablishPrioritiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
